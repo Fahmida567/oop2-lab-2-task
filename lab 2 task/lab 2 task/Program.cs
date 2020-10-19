3c07927c09175d6c3fc39a6118e415a0a0cd0788
Fahmida <fahmidaturin34@gmail.com>
2020-10-19 13:02:55 +0600

    Lab 2 task completed

---
+++ lab 2 task/lab 2 task/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace lab_2_task
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //1
+           /* for (int i = 0; i <= 10; i++)
+            {
+                int x = i % 2;
+                if (x == 0)
+                {
+                    Console.Write(i + " ");
+                }
+
+
+               //2
+
+                int x = 0;
+                for (int i = 0; i < 4; i++)
+                {
+                    for(int j = 0; j <= 5; j++)
+                    {
+                        x = i + j;
+                        if (x%2==0)
+                        {
+                            Console.Write("x");
+                        }
+                        else
+                        {
+                            Console.Write("0");
+                        }
+
+
+                    }
+                    Console.WriteLine();
+                }
+                //3
+               /* int x = 0;
+                for (int i = 1; i <=6; i++)
+                {
+                    x = i;
+                    for (int j = 0; j <= 3; j++)
+                    {
+
+
+                            Console.Write(x+j);
+
+
+
+
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+            }
+            }
+        }
+   // }
+//}
