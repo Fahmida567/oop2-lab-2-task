168f8f6d14d5063b37ddd2aebc8d9f27963f7c46
Fahmida <fahmidaturin34@gmail.com>
2020-10-19 20:06:20 +0600

    lab 2 task updated

--- lab 2 task/lab 2 task/Program.cs
+++ lab 2 task/lab 2 task/Program.cs
@@ -8,113 +8,128 @@ namespace lab_2_task
         static void Main(string[] args)
         {
             //1
-            /* for (int i = 0; i <= 10; i++)
-             {
-                 int x = i % 2;
-                 if (x == 0)
-                 {
-                     Console.Write(i + " ");
-                 }*/
+            /*Console.WriteLine("-----------Question 1 answer-------------");
+            Console.WriteLine();
+            for (int i = 0; i <= 10; i++)
+            {
+                int x = i % 2;
+                if (x == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }*/
 
 
             //2
-
-            /*int x = 0;
-            for (int i = 0; i < 4; i++)
-            {
-                for(int j = 0; j <= 5; j++)
-                {
-                    x = i + j;
-                    if (x%2==0)
-                    {
-                        Console.Write("x");
-                    }
-                    else
-                    {
-                        Console.Write("0");
-                    }
-
-
-                }
-                Console.WriteLine();
-            }*/
-            //3
-            /* int x = 0;
-             for (int i = 1; i <=6; i++)
+            /* Console.WriteLine("-----------Question 2 answer----------");
+             Console.WriteLine();
+             int x = 0;
+             for (int i = 0; i < 4; i++)
              {
-                 x = i;
-                 for (int j = 0; j <= 3; j++)
+                 for(int j = 0; j <= 5; j++)
                  {
-
-
-                         Console.Write(x+j);
-
-
+                     x = i + j;
+                     if (x%2==0)
+                     {
+                         Console.Write("x");
+                     }
+                     else
+                     {
+                         Console.Write("0");
+                     }
 
 
                  }
                  Console.WriteLine();
-             }
-             Console.ReadLine();
-         }
-         }*/
+             }*/
+            //3
+            /* Console.WriteLine("-----------Question 3 answer----------");
+             Console.WriteLine();
+             int x = 0;
+              for (int i = 1; i <=6; i++)
+              {
+                  x = i;
+                  for (int j = 0; j <= 3; j++)
+                  {
+
+
+                          Console.Write(x+j);
+
+
+
+
+                  }
+                  Console.WriteLine();
+              }
+              Console.ReadLine();*/
+
+
             //4.
-            /* int sumOfEven = 0;  
-        int sumOfOdd = 0;
+            /* Console.WriteLine("-----------Question 4 answer----------");
+             Console.WriteLine();
+             int sumOfEven = 0;  
+         int sumOfOdd = 0;
 
-        for (int i = 1; i <= 100; i++)
-        {
-            if (i % 2 == 0)
-                sumOfEven =sumOfEven+i;
-            else
-                sumOfOdd =sumOfOdd+ i;
-        }
+         for (int i = 1; i <= 100; i++)
+         {
+             if (i % 2 == 0)
+                 sumOfEven =sumOfEven+i;
+             else
+                 sumOfOdd =sumOfOdd+ i;
+         }
 
-        Console.WriteLine("The even sum is : " + sumOfEven);
-        Console.WriteLine("The odd sum is : " + sumOfOdd);*/
+         Console.WriteLine("The even sum is : " + sumOfEven);
+         Console.WriteLine("The odd sum is : " + sumOfOdd);*/
 
             //5
-            /*for (int i = 1; i <= 5; i++)
-            {
-                for (int j = 1; j <= 5; j++)
-                {
-                    if (i == j)
-                        Console.Write(i);
-                    else
-                        Console.Write("  ");
-                }
-                Console.WriteLine();
-            }*/
-            //6
-            /* Console.WriteLine("Enter number: ");
-             long a = Convert.ToInt32(Console.ReadLine());
-             long answer = 1;
-             for (int i = 1; i <= a; i++)
+            /* Console.WriteLine("-----------Question 5 answer----------");
+             Console.WriteLine();
+
+             for (int i = 1; i <= 5; i++)
              {
-                 answer *= i;
-             }
-             Console.WriteLine("Factorial is: " + answer);*/
+                 for (int j = 1; j <= 5; j++)
+                 {
+                     if (i == j)
+                         Console.Write(i);
+                     else
+                         Console.Write("  ");
+                 }
+                 Console.WriteLine();
+             }*/
+            //6
+            /* Console.WriteLine("-----------Question 6 answer----------");
+             Console.WriteLine();
+             Console.WriteLine("Enter number: ");
+              long a = Convert.ToInt32(Console.ReadLine());
+              long f = 1;
+              for (int i = 1; i <= a; i++)
+              {
+                  f *= i;
+              }
+              Console.WriteLine("Factorial is:"+f);*/
 
             //7
-           /* for (int i = 1; i <= 10; i++)
-            {
-                if (i <= 5)
-                {
-                    for (int k = 1; k <= i; k++)
-                    {
-                        Console.Write("*");
-                    }
-                }
-                else
-                {
-                    for (int k = 4; k>= i - 5; k--)
-                    {
-                        Console.Write("*");
-                    }
-                }
-                Console.WriteLine();
-            }*/
+            /*Console.WriteLine("-----------Question 7 answer----------");
+            Console.WriteLine();
+            for (int i = 1; i <= 10; i++)
+             {
+                 if (i <= 5)
+                 {
+                     for (int k = 1; k <= i; k++)
+                     {
+                         Console.Write("*");
+                     }
+                 }
+                 else
+                 {
+                     for (int k = 4; k>= i - 5; k--)
+                     {
+                         Console.Write("*");
+                     }
+                 }
+                 Console.WriteLine();
+             }*/
         }
-        }
+    }
        }
 
